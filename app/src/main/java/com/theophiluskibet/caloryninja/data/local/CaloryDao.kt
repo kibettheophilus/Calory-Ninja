c27ec7b5ad70c2e7234e39fd4f6c26f36945cb9a
Theo <kibettheophilus@gmail.com>
2023-02-21 06:25:10 +0300

    add calory dao

---
+++ app/src/main/java/com/theophiluskibet/caloryninja/data/local/CaloryDao.kt
@@ -0,0 +1,11 @@
+package com.theophiluskibet.caloryninja.data.local
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+
+@Dao
+interface CaloryDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun saveCalory(list: List<CaloryEntity>): List<CaloryEntity>
+}
